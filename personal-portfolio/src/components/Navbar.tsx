b053c6db2cef11c26e72bbe96875b85221f42c41
mcsandovall <rasec_oiram@live.com.mx>
2022-09-20 00:21:43 -0500

    tailwind set up n nav-bar

---
+++ personal-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#500] text-gray-300">
+           
+        </div>
+    )
+}
+
+export default Navbar
